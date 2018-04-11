786c2a73589f7f2ec9f272354ee743468401f069
Mariam <38266857+um3on@users.noreply.github.com>
2018-04-10 21:35:06 -0400

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,10 +1,24 @@
 // Select color input
+var color=$("#colorPicker");
+
 // Select size input
+var height = $("#inputHeight");
+var width = $("#inputWidth");
 
 // When size is submitted by the user, call makeGrid()
+$("#sizePicker").submit(makeGrid);
 
 function makeGrid() {
+var grid="";
 
-// Your code goes here!
-
+//creat grid html
+for(var r=0;r<height;r++){
+ grid += "<tr>"
+  for(var c=0;c<width;c++){
+  grid +="<th></th>";
+  }
+  grid += "</tr>";
+}
+//append grid html to table element
+$(grid).appendTo("#pixelCanvas");
 }
